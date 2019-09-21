d56e9a18e9fe3fb1f6c5ed9131aff67bd759d3f3
mohamadAkthamObeidat <mohamadaktham7@gmail.com>
2019-09-21 14:34:31 +0300

    First Commit

---
+++ Screens/Conatct Details/Contact Screen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native'
+const ContactScreen = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    );
+}
+
+export default ContactScreen;
