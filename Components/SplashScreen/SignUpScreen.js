6972713fdf00d886e09bebc21a4a7b82755f75d7
Mohamad Aktham Obeidat <mohamadaktham7@gmail.com>
2019-09-22 15:55:47 +0300

    Back End & Boiler Plate Initialized

---
+++ Components/SplashScreen/SignUpScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native'
+const SplashScreen = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    );
+}
+
+export default SplashScreen;
