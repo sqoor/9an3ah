355551b3a9b995524451b828848421cd063ebce8
Mohamad Aktham Obeidat <mohamadaktham7@gmail.com>
2019-09-26 09:19:44 +0300

    Complete Design App Bar

--- Components/Home/CategoriesList.js
+++ Components/Home/CategoriesList.js
@@ -1,29 +1,36 @@
 import React from 'react';
-import { View, Text, ScrollView, StyleSheet } from 'react-native'
+import { View,  ScrollView, StyleSheet } from 'react-native'
 import CategoryItem from './CategoryItem'
+
+
 const CategoriesList = () => {
     return (
-        <View>
-            <View>
-                <ScrollView
-                    horizontal={true}>
-                        <CategoryItem img='../../assets/Home/parquet.svg' title='بلّيط' />
-                        <CategoryItem img='../../assets/Home/paint-brush.svg' title='دهّين' />
-                        <CategoryItem img='../../assets/Home/crane.svg' title='بنّاء' />
-                        <CategoryItem img='../../assets/Home/delivery-truck.svg' title='ونش' />
-                        <CategoryItem img='../../assets/Home/cargo.svg' title='قصّير' />
-                        <CategoryItem img='../../assets/Home/tap.svg' title='مواسرجي' />
-                        <CategoryItem img='../../assets/Home/7dad.svg' title='حداد' />
-                        <CategoryItem img='../../assets/Home/carpenter.svg' title='نجار' />
-                        <CategoryItem img='../../assets/Home/electrician.svg' title='كهربجي' />
-                </ScrollView>
-            </View>
+        <View style={styles.categoriesList}>
+            <ScrollView horizontal={true}>
+
+                <CategoryItem img={require('../../assets/Home/parquet.png' )}title='بلّيط' />
+                <CategoryItem img={require('../../assets/Home/paint-brush.png')} title='دهّين' />
+                <CategoryItem img={require('../../assets/Home/crane.png' )}title='بنّاء' />
+                <CategoryItem img={require('../../assets/Home/delivery-truck.png')} title='ونش' />
+                <CategoryItem img={require('../../assets/Home/cargo.png' )}title='قصّير' />
+                <CategoryItem img={require('../../assets/Home/tap.png' )}title='مواسرجي' />
+                <CategoryItem img={require('../../assets/Home/Smith.png' )}title='حداد' />
+                <CategoryItem img={require('../../assets/Home/carpenter.png' )}title='نجار' />
+                <CategoryItem img={require('../../assets/Home/electrician.png' )}title='كهربجي' />
+
+            </ScrollView>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    
+    categoriesList : {
+        marginTop: 20,
+        height:'40%',
+        width:'100%',
+        justifyContent: "space-between",
+        alignItems: "center",
+    }
 })
 
 export default CategoriesList;
--- Components/Home/HomeScreen.js
+++ Components/Home/HomeScreen.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, Icon, Image } from 'react-native'
-import CategoryItem from './CategoryItem'
+import { View, Text, StyleSheet, TextInput, ScrollView, Image } from 'react-native'
 import { Dropdown } from 'react-native-material-dropdown';
+import CategoriesList from './CategoriesList';
+import GridList from 'react-native-grid-list';
+import WorkerListItem from './WorkerListItem'
+
 const HomeScreen = () => {
     let data = [
         {
@@ -42,6 +45,18 @@ const HomeScreen = () => {
             value: 'السلط',
         }
     ];
+
+    const items = [
+        { thumbnail: { uri: 'https://lorempixel.com/200/200/animals' } },
+        { thumbnail: { uri: 'https://lorempixel.com/200/200/city' } },
+        { thumbnail: { uri: 'https://lorempixel.com/200/200/nature' } },
+        { thumbnail: { uri: 'https://lorempixel.com/200/200/cats' } },
+    ];
+
+      renderItem = ({ item, index }) => (
+        <WorkerListItem img='' name='' field='' location='' />
+      );
+
     return (
         <View>
             <View style={styles.bar}>
@@ -50,7 +65,17 @@ const HomeScreen = () => {
                     <TextInput style={styles.input} placeholder='ابحث عن فنّي ...' placeholderTextColor='#A7A7A7'></TextInput>
                 </View>
                 <Dropdown label='اختار المحافظة ...' data={data} />
+                <CategoriesList />
             </View>
+            <ScrollView style={styles.container}>
+                <GridList
+                    showSeparator
+                    data={items}
+                    numColumns={2}
+                    renderItem={renderItem}
+                />
+            </ScrollView>
+
         </View >
     );
 }
@@ -84,7 +109,16 @@ const styles = StyleSheet.create({
         width: 40,
         height: 40,
         resizeMode: "contain"
-    }
+    },
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        borderRadius: 10,
+    },
 })
 
 export default HomeScreen;
