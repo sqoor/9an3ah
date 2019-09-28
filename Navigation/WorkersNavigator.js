70af791b6808e8013dd90368049a83f9ca1be152
mohamadAkthamObeidat <mohamadaktham7@gmail.com>
2019-09-28 14:32:20 +0300

    Update WorkersNavigator.js

--- Navigation/WorkersNavigator.js
+++ Navigation/WorkersNavigator.js
@@ -5,11 +5,13 @@ import HomeScreen from '../Components/Home/HomeScreen'
 import LoginScreen from '../Components/Login/LoginScreen'
 import SignUpScreen from '../Components/SignUp/SignUpScreen'
 import EditProfileScreen from '../Components/Profile/EditProfile'
+import ProfileScreen from '../Components/Profile/ProfileScreen'
 
 const WorkersNavigator = createStackNavigator({
     Home: HomeScreen,
     Login: LoginScreen,
     SignUp: SignUpScreen,
+    Profile: ProfileScreen,
     EditProfile: EditProfileScreen
 })
 
