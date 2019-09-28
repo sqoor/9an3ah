1f9e89465606fddec51bd56ab2fbe64a948f4420
mohamadAkthamObeidat <mohamadaktham7@gmail.com>
2019-09-28 14:08:01 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './Components/Home/HomeScreen';
+// import HomeScreen from './Components/Home/HomeScreen';
+import WorkersNavigator from './Navigation/WorkersNavigator';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <HomeScreen/>
 
-    </View>
-  );
+  return <WorkersNavigator />;
 }
 
 const styles = StyleSheet.create({
