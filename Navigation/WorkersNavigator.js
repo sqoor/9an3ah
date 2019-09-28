c8433ede333bc6bbb111974126cafc5169d3e934
mohamadAkthamObeidat <mohamadaktham7@gmail.com>
2019-09-28 12:06:36 +0300

    Create WorkersNavigator.js

---
+++ Navigation/WorkersNavigator.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const WorkersNavigator = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    style
+})
+
+
+export default WorkersNavigator
