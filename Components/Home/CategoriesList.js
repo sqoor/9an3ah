b95632a9a003400aced8def495bbc37593355c11
mohamadAkthamObeidat <mohamadaktham7@gmail.com>
2019-09-28 14:08:06 +0300

    Update CategoriesList.js

--- Components/Home/CategoriesList.js
+++ Components/Home/CategoriesList.js
@@ -1,33 +1,33 @@
 import React from 'react';
-import { View,  ScrollView, StyleSheet } from 'react-native'
+import { View, ScrollView, StyleSheet} from 'react-native'
 import CategoryItem from './CategoryItem'
 
 
-const CategoriesList = () => {
+const CategoriesList = (props) => {
     return (
         <View style={styles.categoriesList}>
             <ScrollView horizontal={true}>
 
-                <CategoryItem img={require('../../assets/Home/parquet.png' )}title='بلّيط' />
-                <CategoryItem img={require('../../assets/Home/paint-brush.png')} title='دهّين' />
-                <CategoryItem img={require('../../assets/Home/crane.png' )}title='بنّاء' />
-                <CategoryItem img={require('../../assets/Home/delivery-truck.png')} title='ونش' />
-                <CategoryItem img={require('../../assets/Home/cargo.png' )}title='قصّير' />
-                <CategoryItem img={require('../../assets/Home/tap.png' )}title='مواسرجي' />
-                <CategoryItem img={require('../../assets/Home/Smith.png' )}title='حداد' />
-                <CategoryItem img={require('../../assets/Home/carpenter.png' )}title='نجار' />
-                <CategoryItem img={require('../../assets/Home/electrician.png' )}title='كهربجي' />
-
+                    <CategoryItem img={require('../../assets/Home/parquet.png')} title='بلّيط'  onFilter={props.onFilter}/>
+                    <CategoryItem img={require('../../assets/Home/paint-brush.png')} title='دهّين'  onFilter={props.onFilter}/>
+                    <CategoryItem img={require('../../assets/Home/crane.png')} title='بنّاء'  onFilter={props.onFilter}/>
+                    <CategoryItem img={require('../../assets/Home/delivery-truck.png')} title='ونش'  onFilter={props.onFilter}/>
+                    <CategoryItem img={require('../../assets/Home/cargo.png')} title='قصّير'  onFilter={props.onFilter}/>
+                    <CategoryItem img={require('../../assets/Home/tap.png')} title='مواسرجي'  onFilter={props.onFilter}/>
+                    <CategoryItem img={require('../../assets/Home/Smith.png')} title='حداد'  onFilter={props.onFilter}/>
+                    <CategoryItem img={require('../../assets/Home/carpenter.png')} title='نجار'  onFilter={props.onFilter}/>
+                    <CategoryItem img={require('../../assets/Home/electrician.png')} title='كهربجي'  onFilter={props.onFilter}/>
+                
             </ScrollView>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    categoriesList : {
+    categoriesList: {
         marginTop: 20,
-        height:'40%',
-        width:'100%',
+        height: '40%',
+        width: '100%',
         justifyContent: "space-between",
         alignItems: "center",
     }
