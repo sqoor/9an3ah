2fee4c3b1f173f0052ade055b4aead89a6802a0a
mohamadAkthamObeidat <mohamadaktham7@gmail.com>
2019-09-28 12:44:38 +0300

    Update WorkersNavigator.js

--- Navigation/WorkersNavigator.js
+++ Navigation/WorkersNavigator.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { createStackNavigator } from 'react-navigation'
+import HomeScreen from '../Components/Home/HomeScreen'
+import LoginScreen from '../Components/Login/LoginScreen'
+import SignUpScreen from '../Components/SignUp/SignUpScreen'
 
-const WorkersNavigator = () => {
-    return (
-        <View>
-            <Text></Text>
-        </View>
-    )
-}
-
-
-const styles = StyleSheet.create({
-    style
+createStackNavigator({
+    Home: HomeScreen,
+    Login: LoginScreen,
+    SignUp: SignUpScreen
 })
-
-
-export default WorkersNavigator
