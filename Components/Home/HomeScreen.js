4d9985ed4aa97d0aa470d85c1bce8f063bdc2212
sqoor <alsqoorAbdullah@gmail.com>
2019-10-05 17:38:26 +0300

    convert to react hooks to detect updated user data

--- Components/Home/HomeScreen.js
+++ Components/Home/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, FlatList } from "react-native";
+import { View, StyleSheet, FlatList, AsyncStorage } from "react-native";
 import Header from "./Header";
 import WorkerListItem from "./WorkerListItem";
 import axios from "axios";
@@ -23,23 +23,25 @@ const HomeScreen = props => {
       });
   }
 
-  // Execute "fetchData" Method Once The Component Open.
-  useEffect(() => fetchData(), []);
-
-  // Handle Workers Filter.
-  const filterWorkers = (filter) => {
-    if (!originalWorkers)
-      return "originalWorkers empty"
+  const filterWorkers = filter => {
+    if (!originalWorkers) return "originalWorkers empty";
 
     const filteredWorkers = originalWorkers.filter(worker => {
-      const locationCondition = filter.location ? worker.location === filter.location : true;
-      const fieldCondition = filter.field ? worker.field === filter.field : true;
+      const locationCondition = filter.location
+        ? worker.location === filter.location
+        : true;
+      const fieldCondition = filter.field
+        ? worker.field === filter.field
+        : true;
 
-      return locationCondition && fieldCondition
-    })
-    setWorkers(filteredWorkers)
-  }
-  
+      return locationCondition && fieldCondition;
+    });
+    setWorkers(filteredWorkers);
+  };
+
+  // Execute "fetchData" Method Once The Component Open.
+  useEffect(() => fetchData(), [props.navigation.getParam("user")]);
+
   return (
     <View>
       <Header {...props} filterWorkers={filterWorkers} />
